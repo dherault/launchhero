cc9d330343f4e6fa5553bcafa44ea0146c60ae3f
David Hérault <dherault@gmail.com>
2025-09-28 19:31:42 +0300

    [app] Add AuthenticationWait to authentication

--- app/src/app/authentication.../layout.tsx
+++ app/src/app/authentication.../layout.tsx
@@ -2,8 +2,10 @@ import composeLayout from '~utils/layout/composeLayout'
 
 import AuthenticationLayout from '~components/authentication/AuthenticationLayout'
 import AuthenticationRedirect from '~components/authentication/AuthenticationRedirect'
+import AuthenticationWait from '~components/authentication/AuthenticationWait'
 
 export default composeLayout(
+  AuthenticationWait,
   AuthenticationRedirect,
   AuthenticationLayout,
 )
