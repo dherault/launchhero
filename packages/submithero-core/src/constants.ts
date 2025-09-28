7256f5188fa47d18707619bfbbd0873b91c59ae1
David Hérault <dherault@gmail.com>
2025-09-28 11:12:34 +0300

    [core] Publish

--- packages/submithero-core/src/constants.ts
+++ packages/submithero-core/src/constants.ts
@@ -7,3 +7,5 @@ export const DEVELOPMENT_FRONTEND_PORT = 5173
 export const APP_URL = process.env.NODE_ENV === 'production'
   ? 'https://submithe.ro'
   : `http://localhost:${DEVELOPMENT_FRONTEND_PORT}`
+
+export const SOCIAL_X_URL = 'https://x.com/dherault111'
