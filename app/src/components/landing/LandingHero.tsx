016207478fd4ecb6f4dfb76b3b301e9084aa4f5c
David Hérault <dherault@gmail.com>
2025-09-28 21:44:09 +0300

    [app] Responsify LandingHero

--- app/src/components/landing/LandingHero.tsx
+++ app/src/components/landing/LandingHero.tsx
@@ -8,10 +8,10 @@ import directories from '~data/directories'
 function LandingHero() {
   return (
     <section className="relative mt-8 mx-auto md:pt-8 px-4 w-full max-w-4xl flex flex-col items-center">
-      <h1 className="text-5xl font-extrabold text-center text-balance">
+      <h1 className="text-3xl md:text-5xl font-extrabold text-center text-balance">
         Launch your product and find your first users
       </h1>
-      <div className="mt-2 text-lg text-neutral-500 text-center text-balance">
+      <div className="mt-2 md:text-lg text-neutral-500 text-center text-balance">
         Launch Hero lets you track your startup submissions across
         {' '}
         {directories.length}
