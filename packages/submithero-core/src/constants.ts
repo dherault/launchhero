34881eb86daf2c582f834227257990d425b017f7
David Hérault <dherault@gmail.com>
2025-09-28 11:09:14 +0300

    [core] Create core package

---
+++ packages/submithero-core/src/constants.ts
@@ -0,0 +1,9 @@
+/* ---
+  URLS
+--- */
+
+export const DEVELOPMENT_FRONTEND_PORT = 5173
+
+export const APP_URL = process.env.NODE_ENV === 'production'
+  ? 'https://submithe.ro'
+  : `http://localhost:${DEVELOPMENT_FRONTEND_PORT}`
