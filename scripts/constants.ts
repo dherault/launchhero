9c63d5d28b8a54f34ac393a50883a6210de526af
David Hérault <dherault@gmail.com>
2025-09-28 11:20:17 +0300

    [root] Edit scripts

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,6 +1,9 @@
 export const DEFAULT_COMMIT_PREFIX = 'root'
 
 export const COMMIT_PREFIX_TO_PATH = {
+  functions: 'app/functions/',
   app: 'app/',
+  core: 'packages/submithero-core/',
+  email: 'packages/submithero-email/',
   docs: 'documentation/',
 }
