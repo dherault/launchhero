a26804c7ba690bf6c88b29b6d98415e95190beb1
David Hérault <dherault@gmail.com>
2025-09-29 19:55:14 +0300

    [core] Publish

--- packages/launchhero-core/src/constants.ts
+++ packages/launchhero-core/src/constants.ts
@@ -4,8 +4,48 @@
 
 export const DEVELOPMENT_FRONTEND_PORT = 5173
 
+export const DEVELOPMENT_BACKEND_PORT = 3003
+
 export const APP_URL = process.env.NODE_ENV === 'production'
   ? 'https://launchhe.ro'
   : `http://localhost:${DEVELOPMENT_FRONTEND_PORT}`
 
+export const API_URL = process.env.NODE_ENV === 'production'
+  ? 'https://api.launchhe.ro'
+  : `http://localhost:${DEVELOPMENT_BACKEND_PORT}`
+
 export const SOCIAL_X_URL = 'https://x.com/dherault111'
+
+/* ---
+  PROJECT
+--- */
+
+export const MAX_PROJECT_NAME_LENGTH = 128
+
+export const MAX_PROJECT_INVITES = 20
+
+/* ---
+  ERROR CODES
+--- */
+
+export const ERROR_CODE_INTERNAL_ERROR = 'INTERNAL_ERROR'
+
+export const ERROR_CODE_BAD_REQUEST = 'BAD_REQUEST'
+
+export const ERROR_CODE_FORBIDDEN = 'FORBIDDEN'
+
+export const ERROR_CODE_NO_BEARER_TOKEN = 'NO_BEARER_TOKEN'
+
+export const ERROR_CODE_INVALID_BEARER_TOKEN = 'INVALID_BEARER_TOKEN'
+
+export const ERROR_CODE_USER_NOT_FOUND = 'USER_NOT_FOUND'
+
+export const ERROR_CODE_PROJECT_NOT_FOUND = 'PROJECT_NOT_FOUND'
+
+export const ERROR_CODE_PROJECT_INVITATION_NOT_FOUND = 'PROJECT_INVITATION_NOT_FOUND'
+
+export const ERROR_CODE_USER_NOT_PROJECT_MEMBER = 'USER_NOT_PROJECT_MEMBER'
+
+export const ERROR_CODE_USER_NOT_PROJECT_ADMIN = 'USER_NOT_PROJECT_ADMIN'
+
+export const ERROR_CODE_STRIPE_CUSTOMER_NOT_FOUND = 'STRIPE_CUSTOMER_NOT_FOUND'
--- packages/launchhero-core/src/types.ts
+++ packages/launchhero-core/src/types.ts
@@ -26,8 +26,19 @@ export type SignInProvider = 'password' | 'google.com'
 
 export type Project = DatabaseResource<{
   name: string
+  imageUrl: string | null
   administratorUserIds: string[]
   memberUserIds: string[]
+  stripeId: string | null
+  stripeLink: string | null
+}>
+
+export type ProjectInvitation = DatabaseResource<{
+  email: string
+  projectId: string
+  projectName: string
+  projectImageUrl: string | null
+  inviterName: string
 }>
 
 /* ---
@@ -44,3 +55,78 @@ export type Directory = {
 }
 
 export type DirectoryTag = 'community'
+
+/* ---
+  STRIPE
+--- */
+
+export interface CustomerData {
+  metadata: {
+    projectId: string
+  }
+}
+
+export interface Price {
+  id: string
+  /**
+   * Whether the price can be used for new purchases.
+   */
+  active: boolean
+  currency: string
+  unitAmount: number | null
+  /**
+   * A brief description of the price.
+   */
+  description: string | null
+  /**
+   * One of `one_time` or `recurring` depending on whether the price is for a one-time purchase or a recurring (subscription) purchase.
+   */
+  type: 'one_time' | 'recurring'
+  /**
+   * The frequency at which a subscription is billed. One of `day`, `week`, `month` or `year`.
+   */
+  interval: 'day' | 'month' | 'week' | 'year' | null
+  /**
+   * The number of intervals (specified in the `interval` attribute) between subscription billings. For example, `interval=month` and `interval_count=3` bills every 3 months.
+   */
+  intervalCount: number | null
+  /**
+   * Default number of trial days when subscribing a customer to this price using [`trial_from_plan=true`](https://stripe.com/docs/api#create_subscription-trial_from_plan).
+   */
+  trialPeriodDays: number | null
+  // billingScheme: Stripe.Price.BillingScheme
+  // tiersMode: Stripe.Price.TiersMode | null
+  // tiers: Stripe.Price.Tier[] | null
+  // recurring: Stripe.Price.Recurring | null
+  // transformQuantity: Stripe.Price.TransformQuantity | null
+  // taxBehavior: Stripe.Price.TaxBehavior | null
+  // metadata: Stripe.Metadata | null
+}
+
+export interface Product {
+  id: string
+  /**
+   * Whether the product is currently available for purchase.
+   */
+  active: boolean
+  /**
+   * The product's name, meant to be displayable to the customer. Whenever this product is sold via a subscription, name will show up on associated invoice line item descriptions.
+   */
+  name: string
+  /**
+   * The product's description, meant to be displayable to the customer. Use this field to optionally store a long form explanation of the product being sold for your own rendering purposes.
+   */
+  description: string | null
+  /**
+   * The role that will be assigned to the user if they are subscribed to this plan.
+   */
+  role: string | null
+  /**
+   * A list of up to 8 URLs of images for this product, meant to be displayable to the customer.
+   */
+  images: Array<string>
+  /**
+   * A list of Prices for this billing product.
+   */
+  prices?: Array<Price>
+}
