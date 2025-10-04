339f880d10a34636bb3a0e19a5f28b334291d3f2
David Hérault <dherault@gmail.com>
2025-10-04 17:44:45 +0300

    [app] Edit AuroraText

--- app/src/components/ui/AuroraText.tsx
+++ app/src/components/ui/AuroraText.tsx
@@ -26,9 +26,6 @@ export const AuroraText = memo(
 
     return (
       <span className={`relative ${className}`}>
-        <span className="sr-only">
-          {children}
-        </span>
         <span
           className="relative animate-aurora bg-[length:200%_auto] bg-clip-text text-transparent"
           style={gradientStyle}
